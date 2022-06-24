cf2896bbb2fd449f24f313531218acec73ff26ba
Eric Mai <ericmai10314@gmail.com>
2022-06-24 19:38:48 -0400

    tweaks

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-scroll";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import "./Footer.css";
 import { Button } from "@mui/material";
 
